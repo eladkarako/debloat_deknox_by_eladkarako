be530c2ba34d4fdd9d2521ad47ec2928e2aaa02d
Elad Karako <eladkarako@users.noreply.github.com>
2020-09-15 15:27:09 +0300

    running '_build.cmd' will compress just the flashable-relevant-content to a zip, and align it. windows' binaries included.

---
+++ resources/_build.cmd
@@ -0,0 +1,20 @@
+::@echo off
+
+pushd "..\"
+
+set "NAME=debloat_deknox_by_eladkarako"
+set "VERSION=2020.09.15"
+set "FILENAME=%NAME%_v%VERSION%"
+
+del /f /q "%FILENAME%_aligned.zip"          1>nul 2>nul
+del /f /q "%FILENAME%.zip.old"              1>nul 2>nul
+ren "%FILENAME%.zip" "%FILENAME%.zip.old"   1>nul 2>nul
+
+call "resources\7z.exe" a -tzip -x!"%FILENAME%.zip" -x!"%FILENAME%.old" -x!".git" -x!".github" -x!"resources" -x!".gitattributes" -x!"LICENSE" -x!"README.md" -y -mmt4 -mx9 -ssw "%FILENAME%.zip" *
+echo.
+call "resources\zipalign.exe" -f -v 4 "%FILENAME%.zip" "%FILENAME%_aligned.zip"
+
+del /f /q "%FILENAME%.zip"                      1>nul 2>nul
+ren "%FILENAME%_aligned.zip" "%FILENAME%.zip"   1>nul 2>nul
+
+pause
