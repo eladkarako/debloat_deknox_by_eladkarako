1c1a455aca904df273f582ddb0e56755daae2b5b
Elad Karako <eladkarako@users.noreply.github.com>
2020-09-15 16:19:35 +0300

    adding wider scope to the versioning (making it 4 parts)

--- resources/_build.cmd
+++ resources/_build.cmd
@@ -3,7 +3,7 @@
 pushd "..\"
 
 set "NAME=debloat_deknox_by_eladkarako"
-set "VERSION=2020.09.15"
+set "VERSION=2020.09.15.1618"
 set "FILENAME=%NAME%_v%VERSION%"
 
 del /f /q "%FILENAME%_aligned.zip"          1>nul 2>nul
